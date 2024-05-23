6a3cd541d8f4dd00a6b123d9f235939214630df7
Carlos Pilla <163558253+OPoppys@users.noreply.github.com>
2024-05-23 15:38:29 -0300

    Add files via upload
    Patch dos for

--- Main.java
+++ Main.java
@@ -17,14 +17,10 @@ public class Main {
 
             int []vetor = new int[numvet];// Criação do vetor
 
-            for(int i = 0; i < numvet; i++){// Esse for preenche os valores dentro do vetor
+            for(int i = 0; i < numvet; i++){// Esse for preenche os valores dentro do vetor// Esse for mostra os valores do vetor para o usuario//Esse for soma o valor de dentro do vetor e o sout mostra para o user
                 System.out.println("Digite números inteiros para preencher os espaços do vetor");
                 vetor[i] = sc.nextInt();
-            }
-            for(int i = 0; i < numvet; i++){// Esse for mostra os valores do vetor para o usuario
                 System.out.println("O espaço do vetor numero " + i +" tem o valor de " + vetor[i]);
-            }
-            for (int i = 0; i < numvet; i++){ // Esse for soma o valor de dentro do vetor e o sout mostra para o user
                 soma += vetor[i];
             }
             System.out.println("A soma do vetor é " + soma);
@@ -34,29 +30,26 @@ public class Main {
             vetmaior = vetor[0];
             vetmenor = vetor[0];
 
-            for(int i = 0; i < numvet; i++){//Nesse for tiramos o maior valor do vetor
+            for(int i = 0; i < numvet; i++){//No primeiro if tiramos o maior valor do vetor//No segundo if tiramos o menor valor do vetor
                 if(vetmaior < vetor[i]) {
                     vetmaior = vetor[i];
                 }
-            }
-            System.out.println("O maior valor do vetor é " + vetmaior);//Aqui informamos ao user o maior valor do vetor
-
-            for(int i = 0; i < numvet; i++) {//Nesse for tiramos o menor valor do vetor
                 if (vetmenor > vetor[i]) {
                     vetmenor = vetor[i];
                 }
             }
+            System.out.println("O maior valor do vetor é " + vetmaior);//Aqui informamos ao user o maior valor do vetor
             System.out.println("O menor valor do vetor é " + vetmenor);//Aqui informamos ao user o menor valor do vetor
 
             for(int i = 0; i < 2; i++){//Aqui substitui-se os dois primeiros valores do vetor
                 System.out.println("Substitua dois valores do vetor: ");
                 vetor[i] = sc.nextInt();
             }
-            for(int i = 0; i < numvet; i++) {// Esse for mostra os valores do vetor para o usuario
-                System.out.println("O espaço do vetor numero " + i + " tem o valor de " + vetor[i]);
-            }
             soma = 0;
-            for (int i = 0; i < numvet; i++){ // Esse for soma o valor de dentro do vetor e depois mostra para o user novamente
+
+            for(int i = 0; i < numvet; i++) {// Esse for mostra os valores do vetor para o usuario//Esse for soma o valor de dentro do vetor e depois mostra para o user novamente
+                System.out.println("O espaço do vetor numero " + i + " tem o valor de " + vetor[i]);
+
                 soma += vetor[i];
             }
             System.out.println("A soma do vetor é " + soma);
